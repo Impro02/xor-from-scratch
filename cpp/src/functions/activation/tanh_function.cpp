0f8331b5fed288dc57aea3c073cf1be9f32a7bac
Maxime MARTIN <69305759+Impro02@users.noreply.github.com>
2024-09-13 14:40:14 +0200

    fix: reduce code in tanh

--- cpp/src/functions/activation/tanh_function.cpp
+++ cpp/src/functions/activation/tanh_function.cpp
@@ -4,14 +4,10 @@
 
 Eigen::MatrixXd TanhFunction::activate(const Eigen::MatrixXd &x) const
 {
-    Eigen::MatrixXd result = x.array().tanh();
-
-    return result;
+    return x.array().tanh();
 }
 
 Eigen::MatrixXd TanhFunction::derivative(const Eigen::MatrixXd &x) const
 {
-    Eigen::MatrixXd result = 1 - x.array().tanh().square();
-
-    return result;
+    return 1 - x.array().tanh().square();
 }
